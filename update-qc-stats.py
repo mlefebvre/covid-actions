298c54aaeeb021bd95b091f715c181f50082f0d4
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2021-04-02 11:49:55 -0400

    Script pour fetcher les stats de radio-canada

---
+++ update-qc-stats.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def parse_history(j):
+    cases = int(j["C"])
+    recovered = int(j["R"])
+    dead = int(j["D"])
+    active = cases - recovered - dead
+    hospital = int(j["H"])
+    icu = int(j["I"])
+    vaccines = int(j["V"])
+
+    return {
+        "cases": cases,
+        "active": active,
+        "recovered": recovered,
+        "dead": dead,
+        "hospital": hospital,
+        "icu": icu,
+        "vaccines": vaccines,
+    }
+
+
+response = requests.get("https://kustom.radio-canada.ca/covid-19/canada_quebec")
+history = response.json()[0]["History"]
+
+today = parse_history(history[0])
+yesterday = parse_history(history[1])
+
+if today != yesterday:
+    with open("daily_stats.txt", "w", encoding="UTF-8") as f:
+        for key, value in today.items():
+            diff = '{0:+}'.format(value - yesterday[key])
+            line = f"{key}: {value} ({diff})"
+            print(f"::set-output name={key}::value ({diff})")
+            f.write(f"{key}: {value} ({diff})\n")
